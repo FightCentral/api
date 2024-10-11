a3467b0ec2e8629c0c946d9e2a4fb6d6c939cda1
Theo Poette <theo.poette@kbrwadventure.comm>
2024-10-11 23:07:12 +0200

    FEAT: docker & typescript setup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
+import pino from 'pino-http'
+import logger from './logger';
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(pino({ logger }))
+
+app.get('/health', (req, res) => {
+  res.send({ status: "ok" });
 });
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+  return logger.info(`Express is listening at http://localhost:${port}`);
 });
