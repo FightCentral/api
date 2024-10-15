92336eb8ffb8ada8ba9ba4bde40803f883020006
Theo <poette.theo@gmail.com>
2024-10-15 19:04:33 +0200

    FEAT: log in on register

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,14 +2,15 @@ import { Response, Router, Request, NextFunction, RequestHandler } from "express
 import passport from "passport";
 import { registerUser, loginUser, generateToken } from "../services/auth.service";
 import { User } from "@prisma/client";
-import RequestWithUser from "@/types/request";
 
 const router = Router();
 
 router.post('/register', async (req, res) => {
   try {
-    const user = await registerUser(req.body.email, req.body.password, req.body.name);
-    res.status(201).json({ message: 'User registered', userId: user.id });
+    await registerUser(req.body.email, req.body.password, req.body.name);
+    const token = await loginUser(req.body.email, req.body.password);
+    res.cookie('token', token, { httpOnly: true });
+    res.json({ token });
   } catch (err: any) {
     res.status(400).json({ error: err?.message });
   }
@@ -39,7 +40,7 @@ router.get(
     if (!user)
       return res.status(400).json({ error: 'Invalid credentials' });
 
-    const token = generateToken({id: user.id, email: user.email});
+    const token = generateToken({ id: user.id, email: user.email });
     res.cookie('token', token, { httpOnly: true });
     res.redirect('/dashboard');
 
